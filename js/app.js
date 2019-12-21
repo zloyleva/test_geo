095958b59fde8cb8cbae76a33a228c27a4fa062b
Oleh Levchenko <zloyleva@gmail.com>
2019-12-21 23:57:22 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function deg2rad(deg) {
 // let url = 'http://courses.dp.ua/api/pb_atm.php';
 let url = 'https://api.privatbank.ua/p24api/infrastructure?json&atm&address=&city=%D0%94%D0%BD%D1%96%D0%BF%D1%80%D0%BE';
 fetch(url, {
-  mode: 'no-cors',
+  //mode: 'no-cors',
   method: 'get',
   headers: {
     'Content-Type': 'application/json',
